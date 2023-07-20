b6e5282abc304535554e9060f3c84188f1cf7b3b
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-07-20 15:38:40 +0300

    Creating models

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,38 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=150)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="dishes"
+    )
+
+    class Meta:
+        ordering = ["name"]
