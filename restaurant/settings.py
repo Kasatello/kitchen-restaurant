79b19082c3bb1f8f32f1993761be5d2cbd9880f2
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-07-22 17:52:50 +0300

    Added flake8 and requrements.txt

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -12,6 +12,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -21,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "debug_toolbar",
     "django_bootstrap5",
     "crispy_forms",
     "crispy_bootstrap5",
@@ -36,6 +40,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "restaurant.urls"
