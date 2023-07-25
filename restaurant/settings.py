bc030cc1249d2979294e4029b557c2e3e08c369d
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-07-25 19:27:04 +0300

    Static managing

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -116,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = "staticfiles/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
