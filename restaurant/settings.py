ef0cea938eb71d28e3628d3e049ef2f6d9abedb3
Nazariy <mac@Nazariys-MacBook-Pro.local>
2023-07-25 20:43:03 +0300

    "Added https://restaurant-mate-zb80.onrender.com/ to AALOWER HOSTS"

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://restaurant-mate-zb80.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "restaurant-mate-zb80.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1"
